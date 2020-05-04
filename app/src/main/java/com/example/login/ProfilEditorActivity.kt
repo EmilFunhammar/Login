998f0022633c5f7afe697067302de4822d65bfa5
EmilFunhammar <emil.funhammar@gmail.com>
2020-05-04 12:02:28 +0200

    navigationBar

---
+++ app/src/main/java/com/example/login/ProfilEditorActivity.kt
@@ -0,0 +1,28 @@
+package com.example.login
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_profil_editor.*
+
+class ProfilEditorActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profil_editor)
+
+
+        val saveButton = findViewById<Button>(R.id.save_Button)
+        saveButton.setOnClickListener {
+            doneEditing()
+        }
+    }
+
+    fun doneEditing(){
+        intent = Intent(this, ProfilActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
---
+++ app/src/main/java/com/example/login/WorkDisplayPageActivity.kt
@@ -0,0 +1,12 @@
+package com.example.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WorkDisplayPageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_work_display_page)
+    }
+}
